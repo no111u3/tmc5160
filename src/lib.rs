7756f815d9d833a6b92cb34f39a72577c4c8326d
Boris Vinogradov <no111u3@gmail.com>
2019-06-24 21:20:43 +0300

    Draft registrer's IO

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,17 @@ pub struct Tmc5160<SPI, CS> {
     cs: CS,
 }
 
+
+#[derive(Debug)]
+pub enum Error<E> {
+    /// SPI bus error
+    Spi(E)
+}
+
+pub trait Address {
+    fn addr(self) -> u8;
+}
+
 impl<SPI, CS, E> Tmc5160<SPI, CS>
 where
     SPI: Transfer<u8, Error = E> + Write<u8, Error = E>,
@@ -33,5 +44,43 @@ where
     /// Create a new driver from a SPI peripheral and a NCS pin
     pub fn new(spi: SPI, cs: CS) -> Result<Self, E> {
         Ok(Tmc5160 { spi, cs })
+    }
+
+    pub fn read_register<T>(&mut self, reg: T) -> Result<u32, Error<E>>
+    where
+        T: Address
+    {
+        self.cs.set_low().ok();
+        
+        let mut buffer = [reg.addr() & 0x7f];
+        
+        self.spi.transfer(&mut buffer).map_err(Error::Spi)?;
+
+        let mut ret_val:[u8; 4] = [0; 4];
+        
+        self.spi.transfer(&mut ret_val).map_err(Error::Spi)?;
+        
+        self.cs.set_high().ok();
+        
+        Ok(u32::from_be_bytes(ret_val))
+    }
+
+    pub fn write_register<T>(&mut self, reg: T, data: u32) -> Result<u8, Error<E>>
+    where
+        T: Address
+    {
+        self.cs.set_low().ok();
+
+        let mut buffer = [reg.addr() | 0x80];
+        
+        self.spi.transfer(&mut buffer).map_err(Error::Spi)?;
+        
+        let mut val = data.to_be_bytes();
+
+        self.spi.transfer(&mut val).map_err(Error::Spi)?;
+        
+        self.cs.set_high().ok();
+
+        Ok(buffer[0])
     }
 }
